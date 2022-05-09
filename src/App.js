baeeb3c6643bd447b93f881e78e53e35dc1727d2
anandcool678 <anand201101dwivedi@gmail.com>
2022-05-10 02:53:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App  container-fluid">
+      <video loop autoplay muted>
+        <source
+          src="https://vimeo.com/707969803"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <div className="heading">
+        <img src={logo} className="logoImg" />
+        <h1>
+          Tussle Fit
+        </h1>
+      </div>
+      <p>
+        Become your best self.
+      </p>
+      <div className="d-flex justify-content-center mt-5">
+      <div className="login p-3">
+        <h6 className="d-flex justify-content-start mb-4">
+          Log in
+        </h6>
+        <div className="d-flex justify-content-start mb-4">
+        <select name="timeframe" id="timeframe" className='countryCode mr-4' >
+          
+          <option value="+91">+91</option>
+        </select>
+        <input type="tel" placeholder="Enter phone number" className="telephone"></input>
+        </div>
+        <div className="btn button">
+          Log-In
+        </div>
+        <div className="p-0 m-0">
+          <p className="contactSupport mt-4 d-flex justify-content-end">
+            contact support
+          </p>
+        </div>
+      </div>
+      </div>
+      <div className="p-3">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tussle.fit is currently in closed Beta.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+        <div className="d-flex flex-wrap justify-content-center footer">
+          <p className="mr-3">
+            contact Support
+          </p>
+          <p className="mr-3">
+            Terms of service
+          </p>
+          <p>Privacy Policy</p>
+        </div>
+      
     </div>
   );
 }
